759d7be8f2acdbe34365ea7a26a02a9b8e1c5027
johnmichaellim12 <johnmichaellim041202@gmail.com>
2024-11-20 14:20:04 +0800

    JSDom complete

---
+++ script.js
@@ -0,0 +1,39 @@
+const body = document.body
+// body.append("Yokoso", "Kira Kira")
+
+// const div = document.createElement('div')
+// div.innerText = "Yokoso"
+// const strong = document.createElement('strong')
+// strong.innerHTML = "Yokoso 2"
+// div.innerHTML = "<strong>Yokoso 2</strong>"
+
+// body.append(div)
+// body.append(strong)
+
+// const div = document.querySelector('div')
+
+// console.log(div.textContent);
+// console.log(div.innerText);
+
+const div = document.querySelector('div')
+const spanHi = document.querySelector("#hi") 
+const spanBye = document.querySelector("#bye")
+
+// console.log(spanHi.setAttribute("title", "AAAAAA"))
+// console.log(spanHi.getAttribute())
+// console.log(spanHi.id = "AAAAAAAA")
+
+// spanHi.removeAttribute("id")
+// console.log(spanHi.dataset.longerName)
+// spanHi.dataset.newName = "hey"
+
+// spanHi.classList.add("new-class")
+// spanHi.classList.toggle("hey", true)
+// spanHi.style.color = "red"
+spanHi.style.backgroundColor = "red"
+background-color
+
+// spanBye.remove()
+// spanHi.remove()
+// div.append(spanBye)
+// div.remove(spanHi)
